899aedc266ac68abd88c413aeb32acb9103a2f3f
Joao <kandrocmatunein@gmail.com>
2022-08-28 16:57:36 -0300

    subindo resultado

--- banco.sql
+++ banco.sql
@@ -3,6 +3,7 @@ CREATE TABLE dados (
     Nome varchar(50),
     Sobrenome varchar(50),
     Endereco varchar(150),
-    Cidade varchar(50),
+    Bairro varchar(50),
+    Matricula varchar(12),
     Host varchar(50)
 );
